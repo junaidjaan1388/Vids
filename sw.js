00c4cb38dc0cc107d6ab2b5b332d1e90c57280de
Junaid Ahmed <112433148+junaidjaan1388@users.noreply.github.com>
2025-10-06 17:15:38 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'video-generator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/style.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
